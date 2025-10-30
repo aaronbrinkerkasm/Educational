a75f544885f44f7961505b9cf350ad5d8025fbf7
aaronbrinkerkasm <aaron.brinker@kasmweb.com>
2025-10-30 14:35:47 -0400

    Update environment variables in next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,14 +4,14 @@ const nextConfig = {
   output: 'export',
   distDir: '../public',
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Data Science lab',
+    description: 'A lab environment for data science.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://aaronbrinkerkasm.github.io/Educational/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/Educational/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
